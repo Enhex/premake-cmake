61667477e208d9aa1a87f2e7e62495fdf65ce809
Jarod42 <Jarod42@users.noreply.github.com>
2023-03-08 22:28:00 +0100

    - Fix some multi-configuration issues. - Fix indentation. - Fix empty test for `cfg.sanitize`.

--- cmake_project.lua
+++ cmake_project.lua
@@ -39,7 +39,7 @@ function m.files(prj)
 	tree.traverse(tr, {
 		onleaf = function(node, depth)
 		
-			_p(depth, '"%s"', path.getrelative(prj.workspace.location, node.abspath))
+			_p(1, '"%s"', path.getrelative(prj.workspace.location, node.abspath))
 
 			-- add generated files
 			for cfg in project.eachconfig(prj) do
@@ -48,7 +48,7 @@ function m.files(prj)
 
 				if p.fileconfig.hasFileSettings(filecfg) then
 					for _, output in ipairs(filecfg.buildoutputs) do
-						_p(depth, '"%s"', path.getrelative(prj.workspace.location, output))
+						_p(1, '"%s"', path.getrelative(prj.workspace.location, output))
 					end
 					break
 				elseif rule then
@@ -60,7 +60,7 @@ function m.files(prj)
 					end
 					local rulecfg = p.context.extent(rule, environ)
 					for _, output in ipairs(rulecfg.buildoutputs) do
-						_p(depth, '"%s"', path.getrelative(prj.workspace.location, output))
+						_p(1, '"%s"', path.getrelative(prj.workspace.location, output))
 					end
 					break
 				end
@@ -117,74 +117,73 @@ function m.generate(prj)
 		_p(2, 'LIBRARY_OUTPUT_DIRECTORY "%s"', path.getrelative(prj.workspace.location, cfg.buildtarget.directory))
 		_p(2, 'RUNTIME_OUTPUT_DIRECTORY "%s"', path.getrelative(prj.workspace.location, cfg.buildtarget.directory))
 		_p(1,')')
-		_p('endif()')
 
 		-- include dirs
 
 		if #cfg.externalincludedirs > 0 then
-			_p('target_include_directories("%s" SYSTEM PRIVATE', prj.name)
+			_p(1, 'target_include_directories("%s" SYSTEM PRIVATE', prj.name)
 			for _, includedir in ipairs(cfg.externalincludedirs) do
-				_x(1, '$<$<CONFIG:%s>:%s>', cmake.cfgname(cfg), includedir)
+				_x(2, '"%s"', includedir)
 			end
-			_p(')')
+			_p(1, ')')
 		end
 		if #cfg.includedirs > 0 then
-			_p('target_include_directories("%s" PRIVATE', prj.name)
+			_p(1, 'target_include_directories("%s" PRIVATE', prj.name)
 			for _, includedir in ipairs(cfg.includedirs) do
-				_x(1, '$<$<CONFIG:%s>:%s>', cmake.cfgname(cfg), includedir)
+				_x(2, '"%s"', includedir)
 			end
-			_p(')')
+			_p(1, ')')
 		end
 
 		if #cfg.forceincludes > 0 then
-			_p('if (MSVC)')
-			_p(1, 'target_compile_options("%s" PRIVATE %s)', prj.name, table.implode(p.tools.msc.getforceincludes(cfg), "", "", " "))
-			_p('else()')
-			_p(1, 'target_compile_options("%s" PRIVATE %s)', prj.name, table.implode(p.tools.gcc.getforceincludes(cfg), "", "", " "))
-			_p('endif()')
+			_p(1, 'if (MSVC)')
+			_p(2, 'target_compile_options("%s" PRIVATE %s)', prj.name, table.implode(p.tools.msc.getforceincludes(cfg), "", "", " "))
+			_p(1, 'else()')
+			_p(2, 'target_compile_options("%s" PRIVATE %s)', prj.name, table.implode(p.tools.gcc.getforceincludes(cfg), "", "", " "))
+			_p(1, 'endif()')
 		end
 
 		-- defines
 		if #cfg.defines > 0 then
-			_p('target_compile_definitions("%s" PRIVATE', prj.name)
+			_p(1, 'target_compile_definitions("%s" PRIVATE', prj.name)
 			for _, define in ipairs(cfg.defines) do
-				_p(1, '$<$<CONFIG:%s>:%s>', cmake.cfgname(cfg), p.esc(define):gsub(' ', '\\ '))
+				_p(2, '"%s"', p.esc(define):gsub(' ', '\\ '))
 			end
-			_p(')')
+			_p(1, ')')
 		end
 
 		-- lib dirs
 		if #cfg.libdirs > 0 then
-			_p('target_link_directories("%s" PRIVATE', prj.name)
+			_p(1, 'target_link_directories("%s" PRIVATE', prj.name)
 			for _, libdir in ipairs(cfg.libdirs) do
-				_p(1, '$<$<CONFIG:%s>:%s>', cmake.cfgname(cfg), libdir)
+				_p(2, '"%s"', libdir)
 			end
-			_p(')')
+			_p(1, ')')
 		end
 
 		-- libs
 		local uselinkgroups = isclangorgcc and cfg.linkgroups == p.ON
 		if uselinkgroups or # config.getlinks(cfg, "dependencies", "object") > 0 or #config.getlinks(cfg, "system", "fullpath") > 0 then
-			_p('target_link_libraries("%s"', prj.name)
+			_p(1, 'target_link_libraries("%s"', prj.name)
 			-- Do not use toolset here as cmake needs to resolve dependency chains
 			if uselinkgroups then
-				_p(1, '-Wl,--start-group')
+				_p(2, '-Wl,--start-group')
 			end
 			for a, link in ipairs(config.getlinks(cfg, "dependencies", "object")) do
-				_p(1, '$<$<CONFIG:%s>:%s>', cmake.cfgname(cfg), link.project.name)
+				_p(2, '"%s"', link.project.name)
 			end
 			if uselinkgroups then
 				-- System libraries don't depend on the project
-				_p(1, '-Wl,--end-group')
-				_p(1, '-Wl,--start-group')
+				_p(2, '-Wl,--end-group')
+				_p(2, '-Wl,--start-group')
 			end
 			for _, link in ipairs(config.getlinks(cfg, "system", "fullpath")) do
-				_p(1, '$<$<CONFIG:%s>:%s>', cmake.cfgname(cfg), link)
+				_p(2, '"%s"', link)
 			end
 			if uselinkgroups then
-				_p(1, '-Wl,--end-group')
+				_p(2, '-Wl,--end-group')
 			end
-			_p(')')
+			_p(1, ')')
 		end
 
 		-- setting build options
@@ -194,9 +193,9 @@ function m.generate(prj)
 		end
 		
 		if all_build_options ~= "" then
-			_p('if(CMAKE_BUILD_TYPE STREQUAL %s)', cmake.cfgname(cfg))
-			_p(1, 'set_target_properties("%s" PROPERTIES COMPILE_FLAGS %s)', prj.name, all_build_options)
-			_p('endif()')
+			_p(1, 'if(CMAKE_BUILD_TYPE STREQUAL %s)', cmake.cfgname(cfg))
+			_p(2, 'set_target_properties("%s" PROPERTIES COMPILE_FLAGS %s)', prj.name, all_build_options)
+			_p(1, 'endif()')
 		end
 
 		-- setting link options
@@ -205,12 +204,11 @@ function m.generate(prj)
 			all_link_options = all_link_options .. option .. " "
 		end
 
-		if all_link_options ~= "" or cfg.sanitize then
-			_p('if(CMAKE_BUILD_TYPE STREQUAL %s)', cmake.cfgname(cfg))
+		if all_link_options ~= "" or (cfg.sanitize and #cfg.sanitize ~= 0) then
 			if all_link_options ~= "" then
 				_p(1, 'set_target_properties("%s" PROPERTIES LINK_FLAGS "%s")', prj.name, all_link_options)
 			end
-			if cfg.sanitize then
+			if cfg.sanitize and #cfg.sanitize ~= 0 then
 				_p(1, 'if (NOT MSVC)')
 				if table.contains(cfg.sanitize, "Address") then
 					_p(2, 'target_link_options("%s" PRIVATE "-fsanitize=address")', prj.name)
@@ -220,29 +218,28 @@ function m.generate(prj)
 				end
 				_p(1, 'endif()')
 			end
-			_p('endif()')
 		end
 
 		if #toolset.getcflags(cfg) > 0 or #toolset.getcxxflags(cfg) > 0 then
-			_p('if (MSVC)')
-			_p(1, 'target_compile_options("%s" PRIVATE', prj.name)
+			_p(1, 'if (MSVC)')
+			_p(2, 'target_compile_options("%s" PRIVATE', prj.name)
 			for _, flag in ipairs(p.tools.msc.getcflags(cfg)) do
-				_p(2, '$<$<AND:$<CONFIG:%s>,$<COMPILE_LANGUAGE:C>>:%s>', cmake.cfgname(cfg), flag)
+				_p(3, '$<$<COMPILE_LANGUAGE:C>:%s>', flag)
 			end
 			for _, flag in ipairs(p.tools.msc.getcxxflags(cfg)) do
-				_p(2, '$<$<AND:$<CONFIG:%s>,$<COMPILE_LANGUAGE:CXX>>:%s>', cmake.cfgname(cfg), flag)
+				_p(3, '$<$<COMPILE_LANGUAGE:CXX>:%s>', flag)
 			end
-			_p(1, ')')
-			_p('else()')
-			_p(1, 'target_compile_options("%s" PRIVATE', prj.name)
+			_p(2, ')')
+			_p(1, 'else()')
+			_p(2, 'target_compile_options("%s" PRIVATE', prj.name)
 			for _, flag in ipairs(p.tools.gcc.getcflags(cfg)) do
-				_p(2, '$<$<AND:$<CONFIG:%s>,$<COMPILE_LANGUAGE:C>>:%s>', cmake.cfgname(cfg), flag)
+				_p(3, '$<$<COMPILE_LANGUAGE:C>:%s>', flag)
 			end
 			for _, flag in ipairs(p.tools.gcc.getcxxflags(cfg)) do
-				_p(2, '$<$<AND:$<CONFIG:%s>,$<COMPILE_LANGUAGE:CXX>>:%s>', cmake.cfgname(cfg), flag)
+				_p(3, '$<$<COMPILE_LANGUAGE:CXX>:%s>', flag)
 			end
-			_p(1, ')')
-			_p('endif()')
+			_p(2, ')')
+			_p(1, 'endif()')
 		end
 
 		-- C++ standard
@@ -264,7 +261,6 @@ function m.generate(prj)
 			local pic = iif(cfg.pic == 'On', 'True', 'False')
 			local lto = iif(cfg.flags.LinkTimeOptimization, 'True', 'False')
 
-			_p('if(CMAKE_BUILD_TYPE STREQUAL %s)', cmake.cfgname(cfg))
 			_p(1, 'set_target_properties("%s" PROPERTIES', prj.name)
 			_p(2, 'CXX_STANDARD %s', standard[cfg.cppdialect])
 			_p(2, 'CXX_STANDARD_REQUIRED YES')
@@ -272,7 +268,6 @@ function m.generate(prj)
 			_p(2, 'POSITION_INDEPENDENT_CODE %s', pic)
 			_p(2, 'INTERPROCEDURAL_OPTIMIZATION %s', lto)
 			_p(1, ')')
-			_p('endif()')
 		end
 
 		-- precompiled headers
@@ -302,41 +297,38 @@ function m.generate(prj)
 				pch = project.getrelative(cfg.project, path.getabsolute(pch))
 			end
 
-			_p('if(CMAKE_BUILD_TYPE STREQUAL %s)', cmake.cfgname(cfg))
-			_p('target_precompile_headers("%s" PUBLIC %s)', prj.name, pch)
-			_p('endif()')
+			_p(1, 'target_precompile_headers("%s" PUBLIC "%s")', prj.name, pch)
 		end
 
 		-- pre/post buildcommands
 		if cfg.prebuildmessage or #cfg.prebuildcommands > 0 then
 			-- add_custom_command PRE_BUILD runs just before generating the target
 			-- so instead, use add_custom_target to run it before any rule (as obj)
-			_p('add_custom_target(prebuild-%s', prj.name)
+			_p(1, 'add_custom_target(prebuild-%s', prj.name)
 			if cfg.prebuildmessage then
 				local command = os.translateCommandsAndPaths("{ECHO} " .. m.quote(cfg.prebuildmessage), cfg.project.basedir, cfg.project.location)
-				_p('  COMMAND %s', command)
+				_p(2, 'COMMAND %s', command)
 			end
 			local commands = os.translateCommandsAndPaths(cfg.prebuildcommands, cfg.project.basedir, cfg.project.location)
 			for _, command in ipairs(commands) do
-				_p('  COMMAND %s', command)
+				_p(2, 'COMMAND %s', command)
 			end
-			_p(')')
-			_p('add_dependencies(%s prebuild-%s)', prj.name, prj.name)
+			_p(1, ')')
+			_p(1, 'add_dependencies(%s prebuild-%s)', prj.name, prj.name)
 		end
 
 		if cfg.postbuildmessage or #cfg.postbuildcommands > 0 then
-			_p('add_custom_command(TARGET %s POST_BUILD', prj.name)
+			_p(1, 'add_custom_command(TARGET %s POST_BUILD', prj.name)
 			if cfg.postbuildmessage then
 				local command = os.translateCommandsAndPaths("{ECHO} " .. m.quote(cfg.postbuildmessage), cfg.project.basedir, cfg.project.location)
-				_p('  COMMAND %s', command)
+				_p(2, 'COMMAND %s', command)
 			end
 			local commands = os.translateCommandsAndPaths(cfg.postbuildcommands, cfg.project.basedir, cfg.project.location)
 			for _, command in ipairs(commands) do
-				_p('  COMMAND %s', command)
+				_p(2, 'COMMAND %s', command)
 			end
-			_p(')')
+			_p(1, ')')
 		end
-
 		-- custom command
 		local function addCustomCommand(fileconfig, filename)
 			if #fileconfig.buildcommands == 0 or #fileconfig.buildoutputs == 0 then
@@ -345,22 +337,22 @@ function m.generate(prj)
 
 			local custom_output_directories = table.unique(table.translate(fileconfig.buildoutputs, function(output) return project.getrelative(cfg.project, path.getdirectory(output)) end))
 			-- Alternative would be to add 'COMMAND ${CMAKE_COMMAND} -E make_directory %s' to below add_custom_command
-			_p('file(MAKE_DIRECTORY %s)', table.implode(custom_output_directories, "", "", " "))
+			_p(1, 'file(MAKE_DIRECTORY %s)', table.implode(custom_output_directories, "", "", " "))
 
-			_p('add_custom_command(TARGET OUTPUT %s', table.implode(project.getrelative(cfg.project, fileconfig.buildoutputs),"",""," "))
+			_p(1, 'add_custom_command(TARGET OUTPUT %s', table.implode(project.getrelative(cfg.project, fileconfig.buildoutputs),"",""," "))
 			if fileconfig.buildmessage then
-				_p('  COMMAND %s', os.translateCommandsAndPaths('{ECHO} ' .. m.quote(fileconfig.buildmessage), cfg.project.basedir, cfg.project.location))
+				_p(2, 'COMMAND %s', os.translateCommandsAndPaths('{ECHO} ' .. m.quote(fileconfig.buildmessage), cfg.project.basedir, cfg.project.location))
 			end
 			for _, command in ipairs(fileconfig.buildcommands) do
-				_p('  COMMAND %s', os.translateCommandsAndPaths(command, cfg.project.basedir, cfg.project.location))
+				_p(2, 'COMMAND %s', os.translateCommandsAndPaths(command, cfg.project.basedir, cfg.project.location))
 			end
 			if filename ~= "" and #fileconfig.buildinputs ~= 0 then
 				filename = filename .. " "
 			end
 			if filename ~= "" or #fileconfig.buildinputs ~= 0 then
-				_p('  DEPENDS %s', filename .. table.implode(fileconfig.buildinputs,"",""," "))
+				_p(2, 'DEPENDS %s', filename .. table.implode(fileconfig.buildinputs,"",""," "))
 			end
-			_p(')')
+			_p(1, ')')
 		end
 		local tr = project.getsourcetree(cfg.project)
 		p.tree.traverse(tr, {
@@ -383,6 +375,8 @@ function m.generate(prj)
 			end
 		})
 		addCustomCommand(cfg, "")
+		_p('endif()')
+		_p('')
 	end
 -- restore
 	path.getDefaultSeparator = oldGetDefaultSeparator
